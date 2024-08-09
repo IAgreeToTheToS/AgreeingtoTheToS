5121cdab3e2eee4b6e95b728763dd75decf31c50
IAgreeToTheToS <161591935+IAgreeToTheToS@users.noreply.github.com>
2024-08-09 13:24:40 +1000

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,3 @@ let element = {
     "button": document.getElementById("agree-btn"),
     "banner": document.getElementById("banner")
 };
-function checkForClose() {
-    let checkbox = document.getElementById("agree-checkbox").checked
-    if (checkbox) {
-        element.button.addEventListener("click", function () {
-            element.banner.remove();
-            clearInterval(checkForCloseInterval);
-        });
-    }
-}
-let checkForCloseInterval = setInterval(checkForClose, 100);
